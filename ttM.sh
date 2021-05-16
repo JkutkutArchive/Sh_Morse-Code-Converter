cb7f12fae7cf876e12f29d0eb0acfe35d819e252
jkutkut <jorgeregonzalez@gmail.com>
2021-05-17 01:01:31 +0200

    fast way to convert

---
+++ ttM.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+declare -A conversor;
+conversor[a]=".-";
+conversor[b]="-...";
+conversor[c]="-.-.";
+conversor[d]="-..";
+conversor[e]=".";
+conversor[f]="..-.";
+conversor[g]="--.";
+conversor[h]="....";
+conversor[i]="..";
+conversor[j]=".---";
+conversor[k]="-.-";
+conversor[l]=".-..";
+conversor[m]="--";
+conversor[n]="-.";
+conversor[o]="---";
+conversor[p]=".--.";
+conversor[q]="--.-";
+conversor[r]=".-.";
+conversor[s]="...";
+conversor[t]="-";
+conversor[u]="..-";
+conversor[v]="...-";
+conversor[w]=".--";
+conversor[x]="-..-";
+conversor[y]="-.--";
+conversor[z]="--..";
+conversor[0]="-----";
+conversor[1]=".----";
+conversor[2]="..---";
+conversor[3]="...--";
+conversor[4]="....-";
+conversor[5]=".....";
+conversor[6]="-....";
+conversor[7]="--...";
+conversor[8]="---..";
+conversor[9]="----.";
+conversor[ ]="      ";
+
+
+if [[ $@ != "" ]]; then
+    textToConvert=$@; # Arguments are the text
+fi
+
+for (( i = 0; i < ${#textToConvert}; i++ )); do
+    if [[ ${textToConvert:$i:1} = " " ]] || [[ ${msg:(( ${#msg} - 1 )):1} = "|" ]]; then
+        # Because of space between words is | (7 spaces), do not add extra spaces in the sides
+        msg+="${conversor[${textToConvert:$i:1}]}";
+    else
+        msg+=" ${conversor[${textToConvert:$i:1}]}"; # 3 spaces = space between characters
+    fi
+done
+msg="${msg:1:((${#msg} - 1))}";
+echo "$msg";
+printf "$msg" | xclip -i -selection clipboard;
