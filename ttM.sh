52ddc8519a483c626ca5c98532b73469eb819d18
jkutkut <jorgeregonzalez@gmail.com>
2021-05-17 12:51:37 +0200

    in theory, now correct and reduced modes should work

--- ttM.sh
+++ ttM.sh
@@ -5,7 +5,8 @@ NC='\033[0m'; # No Color
 RED='\033[0;31m';
 
 declare -A conversor;
-# if [[ $1 == "-r" ]]; then
+if [[ $1 == "-r" ]]; then
+    correctMode=1;
     spacer="   ";
     conversor[a]=". -";
     conversor[b]="- . . .";
@@ -44,56 +45,68 @@ declare -A conversor;
     conversor[8]="- - - . .";
     conversor[9]="- - - - .";
     conversor[ ]=" ";
-# else
-#     spacer=" ";
-#     conversor[a]=".-";
-#     conversor[b]="-...";
-#     conversor[c]="-.-.";
-#     conversor[d]="-..";
-#     conversor[e]=".";
-#     conversor[f]="..-.";
-#     conversor[g]="--.";
-#     conversor[h]="....";
-#     conversor[i]="..";
-#     conversor[j]=".---";
-#     conversor[k]="-.-";
-#     conversor[l]=".-..";
-#     conversor[m]="--";
-#     conversor[n]="-.";
-#     conversor[o]="---";
-#     conversor[p]=".--.";
-#     conversor[q]="--.-";
-#     conversor[r]=".-.";
-#     conversor[s]="...";
-#     conversor[t]="-";
-#     conversor[u]="..-";
-#     conversor[v]="...-";
-#     conversor[w]=".--";
-#     conversor[x]="-..-";
-#     conversor[y]="-.--";
-#     conversor[z]="--..";
-#     conversor[0]="-----";
-#     conversor[1]=".----";
-#     conversor[2]="..---";
-#     conversor[3]="...--";
-#     conversor[4]="....-";
-#     conversor[5]=".....";
-#     conversor[6]="-....";
-#     conversor[7]="--...";
-#     conversor[8]="---..";
-#     conversor[9]="----.";
-#     conversor[ ]="   ";
-# fi
+else
+    correctMode=0;
+    spacer=" ";
+    conversor[a]=".-";
+    conversor[b]="-...";
+    conversor[c]="-.-.";
+    conversor[d]="-..";
+    conversor[e]=".";
+    conversor[f]="..-.";
+    conversor[g]="--.";
+    conversor[h]="....";
+    conversor[i]="..";
+    conversor[j]=".---";
+    conversor[k]="-.-";
+    conversor[l]=".-..";
+    conversor[m]="--";
+    conversor[n]="-.";
+    conversor[o]="---";
+    conversor[p]=".--.";
+    conversor[q]="--.-";
+    conversor[r]=".-.";
+    conversor[s]="...";
+    conversor[t]="-";
+    conversor[u]="..-";
+    conversor[v]="...-";
+    conversor[w]=".--";
+    conversor[x]="-..-";
+    conversor[y]="-.--";
+    conversor[z]="--..";
+    conversor[0]="-----";
+    conversor[1]=".----";
+    conversor[2]="..---";
+    conversor[3]="...--";
+    conversor[4]="....-";
+    conversor[5]=".....";
+    conversor[6]="-....";
+    conversor[7]="--...";
+    conversor[8]="---..";
+    conversor[9]="----.";
+    conversor[ ]=" ";
+fi
 
-
-if [[ $@ != "" ]]; then
-    if [[ $1 == "-f" ]]; then
-        textToConvert=$(cat $2);
+if [[ $correctMode == 1 ]]; then
+    if [[ ${@:2} != "" ]]; then
+        if [[ $2 == "-f" ]]; then
+            textToConvert=$(cat $3);
+        else
+            textToConvert=${@:2}; # Arguments are the text
+        fi
     else
-        textToConvert=$@; # Arguments are the text
+        textToConvert=$(cat);
     fi
 else
-    textToConvert=$(cat);
+    if [[ $@ != "" ]]; then
+        if [[ $1 == "-f" ]]; then
+            textToConvert=$(cat $2);
+        else
+            textToConvert=$@; # Arguments are the text
+        fi
+    else
+        textToConvert=$(cat);
+    fi
 fi
 
 reEx='^[a-z 0-9]*$';
