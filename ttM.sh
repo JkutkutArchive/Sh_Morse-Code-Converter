ff9545e052af3599a8067455d1186f060471beef
jkutkut <jorgeregonzalez@gmail.com>
2021-05-17 12:46:07 +0200

    now the code can convert to correct morse code

--- ttM.sh
+++ ttM.sh
@@ -5,43 +5,85 @@ NC='\033[0m'; # No Color
 RED='\033[0;31m';
 
 declare -A conversor;
-conversor[a]=".-";
-conversor[b]="-...";
-conversor[c]="-.-.";
-conversor[d]="-..";
-conversor[e]=".";
-conversor[f]="..-.";
-conversor[g]="--.";
-conversor[h]="....";
-conversor[i]="..";
-conversor[j]=".---";
-conversor[k]="-.-";
-conversor[l]=".-..";
-conversor[m]="--";
-conversor[n]="-.";
-conversor[o]="---";
-conversor[p]=".--.";
-conversor[q]="--.-";
-conversor[r]=".-.";
-conversor[s]="...";
-conversor[t]="-";
-conversor[u]="..-";
-conversor[v]="...-";
-conversor[w]=".--";
-conversor[x]="-..-";
-conversor[y]="-.--";
-conversor[z]="--..";
-conversor[0]="-----";
-conversor[1]=".----";
-conversor[2]="..---";
-conversor[3]="...--";
-conversor[4]="....-";
-conversor[5]=".....";
-conversor[6]="-....";
-conversor[7]="--...";
-conversor[8]="---..";
-conversor[9]="----.";
-conversor[ ]="   ";
+# if [[ $1 == "-r" ]]; then
+    spacer="   ";
+    conversor[a]=". -";
+    conversor[b]="- . . .";
+    conversor[c]="- . - .";
+    conversor[d]="- . .";
+    conversor[e]=".";
+    conversor[f]=". . - .";
+    conversor[g]="- - .";
+    conversor[h]=". . . .";
+    conversor[i]=". .";
+    conversor[j]=". - - -";
+    conversor[k]="- . -";
+    conversor[l]=". - . .";
+    conversor[m]="- -";
+    conversor[n]="- .";
+    conversor[o]="- - -";
+    conversor[p]=". - - .";
+    conversor[q]="- - . -";
+    conversor[r]=". - .";
+    conversor[s]=". . .";
+    conversor[t]="-";
+    conversor[u]=". . -";
+    conversor[v]=". . . -";
+    conversor[w]=". - -";
+    conversor[x]="- . . -";
+    conversor[y]="- . - -";
+    conversor[z]="- - . .";
+    conversor[0]="- - - - -";
+    conversor[1]=". - - - -";
+    conversor[2]=". . - - -";
+    conversor[3]=". . . - -";
+    conversor[4]=". . . . -";
+    conversor[5]=". . . . .";
+    conversor[6]="- . . . .";
+    conversor[7]="- - . . .";
+    conversor[8]="- - - . .";
+    conversor[9]="- - - - .";
+    conversor[ ]=" ";
+# else
+#     spacer=" ";
+#     conversor[a]=".-";
+#     conversor[b]="-...";
+#     conversor[c]="-.-.";
+#     conversor[d]="-..";
+#     conversor[e]=".";
+#     conversor[f]="..-.";
+#     conversor[g]="--.";
+#     conversor[h]="....";
+#     conversor[i]="..";
+#     conversor[j]=".---";
+#     conversor[k]="-.-";
+#     conversor[l]=".-..";
+#     conversor[m]="--";
+#     conversor[n]="-.";
+#     conversor[o]="---";
+#     conversor[p]=".--.";
+#     conversor[q]="--.-";
+#     conversor[r]=".-.";
+#     conversor[s]="...";
+#     conversor[t]="-";
+#     conversor[u]="..-";
+#     conversor[v]="...-";
+#     conversor[w]=".--";
+#     conversor[x]="-..-";
+#     conversor[y]="-.--";
+#     conversor[z]="--..";
+#     conversor[0]="-----";
+#     conversor[1]=".----";
+#     conversor[2]="..---";
+#     conversor[3]="...--";
+#     conversor[4]="....-";
+#     conversor[5]=".....";
+#     conversor[6]="-....";
+#     conversor[7]="--...";
+#     conversor[8]="---..";
+#     conversor[9]="----.";
+#     conversor[ ]="   ";
+# fi
 
 
 if [[ $@ != "" ]]; then
@@ -62,8 +104,8 @@ if [[ ! "$textToConvert" =~ $reEx ]]; then
 fi
 
 for (( i = 0; i < ${#textToConvert}; i++ )); do
-    msg+=" ${conversor[${textToConvert:$i:1}]}"; # 3 spaces = space between characters 
+    msg+="$spacer${conversor[${textToConvert:$i:1}]}"; # 3 spaces = space between characters 
 done
-msg="${msg:1:((${#msg} - 1))}";
+msg="${msg:${#spacer}:((${#msg} - 1))}";
 echo "$msg";
 printf "$msg" | xclip -i -selection clipboard;
