c3d924326966e321fc436f4e8f7bba55ba179b30
jkutkut <jorgeregonzalez@gmail.com>
2021-05-18 12:26:44 +0200

    now able to execute the script

--- testValidation.sh
+++ testValidation.sh
@@ -1,5 +1,11 @@
 echo "Starting test";
 test1="abcdefghijklmnopkrstuvwxyz 0123456789";
-morseR=$(./ttM.sh $test1);
+echo "Testing: $test1";
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $__dir;
+
+
+( . ./ttM.sh $test1;)
+echo "done $msg"
 echo ${morseR};
 # echo $morseR
