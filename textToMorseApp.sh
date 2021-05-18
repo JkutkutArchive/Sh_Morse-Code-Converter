7cce881f576f30791607f87dc9793ccf03abca3b
jkutkut <jorgeregonzalez@gmail.com>
2021-05-19 00:07:33 +0200

    updated with some of the logic used on the ttM.sh file

--- textToMorseApp.sh
+++ textToMorseApp.sh
@@ -9,43 +9,87 @@ YELLOW='\033[1;33m';
 LBLUE='\033[1;34m';
 
 declare -A conversor;
-conversor[a]=".-";
-conversor[b]="-...";
-conversor[c]="-.-.";
-conversor[d]="-..";
-conversor[e]=".";
-conversor[f]="..-.";
-conversor[g]="--.";
-conversor[h]="....";
-conversor[i]="..";
-conversor[j]=".---";
-conversor[k]="-.-";
-conversor[l]=".-..";
-conversor[m]="--";
-conversor[n]="-.";
-conversor[o]="---";
-conversor[p]=".--.";
-conversor[q]="--.-";
-conversor[r]=".-.";
-conversor[s]="...";
-conversor[t]="-";
-conversor[u]="..-";
-conversor[v]="...-";
-conversor[w]=".--";
-conversor[x]="-..-";
-conversor[y]="-.--";
-conversor[z]="--..";
-conversor[0]="-----";
-conversor[1]=".----";
-conversor[2]="..---";
-conversor[3]="...--";
-conversor[4]="....-";
-conversor[5]=".....";
-conversor[6]="-....";
-conversor[7]="--...";
-conversor[8]="---..";
-conversor[9]="----.";
-conversor[ ]="      ";
+if [[ $1 == "-r" ]]; then # if real morse selected
+    nextInput=2;
+    spacer="   ";
+    conversor[a]=". -";
+    conversor[b]="- . . .";
+    conversor[c]="- . - .";
+    conversor[d]="- . .";
+    conversor[e]=".";
+    conversor[f]=". . - .";
+    conversor[g]="- - .";
+    conversor[h]=". . . .";
+    conversor[i]=". .";
+    conversor[j]=". - - -";
+    conversor[k]="- . -";
+    conversor[l]=". - . .";
+    conversor[m]="- -";
+    conversor[n]="- .";
+    conversor[o]="- - -";
+    conversor[p]=". - - .";
+    conversor[q]="- - . -";
+    conversor[r]=". - .";
+    conversor[s]=". . .";
+    conversor[t]="-";
+    conversor[u]=". . -";
+    conversor[v]=". . . -";
+    conversor[w]=". - -";
+    conversor[x]="- . . -";
+    conversor[y]="- . - -";
+    conversor[z]="- - . .";
+    conversor[0]="- - - - -";
+    conversor[1]=". - - - -";
+    conversor[2]=". . - - -";
+    conversor[3]=". . . - -";
+    conversor[4]=". . . . -";
+    conversor[5]=". . . . .";
+    conversor[6]="- . . . .";
+    conversor[7]="- - . . .";
+    conversor[8]="- - - . .";
+    conversor[9]="- - - - .";
+    conversor[ ]=" ";
+else # If reduced morse selected (DEFAULT)
+    nextInput=1;
+    spacer=" ";
+    conversor[a]=".-";
+    conversor[b]="-...";
+    conversor[c]="-.-.";
+    conversor[d]="-..";
+    conversor[e]=".";
+    conversor[f]="..-.";
+    conversor[g]="--.";
+    conversor[h]="....";
+    conversor[i]="..";
+    conversor[j]=".---";
+    conversor[k]="-.-";
+    conversor[l]=".-..";
+    conversor[m]="--";
+    conversor[n]="-.";
+    conversor[o]="---";
+    conversor[p]=".--.";
+    conversor[q]="--.-";
+    conversor[r]=".-.";
+    conversor[s]="...";
+    conversor[t]="-";
+    conversor[u]="..-";
+    conversor[v]="...-";
+    conversor[w]=".--";
+    conversor[x]="-..-";
+    conversor[y]="-.--";
+    conversor[z]="--..";
+    conversor[0]="-----";
+    conversor[1]=".----";
+    conversor[2]="..---";
+    conversor[3]="...--";
+    conversor[4]="....-";
+    conversor[5]=".....";
+    conversor[6]="-....";
+    conversor[7]="--...";
+    conversor[8]="---..";
+    conversor[9]="----.";
+    conversor[ ]=" ";
+fi
 
 reEx='^[a-z 0-9]*$';
 firstTime=1;
@@ -58,14 +102,20 @@ ask(){ # Ask the argument on terminal, save the response in askResponse
 }
 
 
+printf "${BLUE}  __  __                                                     _           
+ |  \/  | ___  _ __ ___  ___    ___ ___  _ ____   _____ _ __| |_ ___ _ __ 
+ | \  / |/ _ \| '__/ __|/ _ \  / __/ _ \| '_ \ \ / / _ \ '__| __/ _ \ '__|
+ | |\/| | (_) | |  \__ \  __/ | (_| (_) | | | \ V /  __/ |  | ||  __/ |   
+ |_|  |_|\___/|_|  |___/\___|  \___\___/|_| |_|\_/ \___|_|   \__\___|_|\n\n\n${NC}"
+
 while [ 1 ]; do
-    if [[ $@ == "" ]] || [[ $firstTime == 0 ]]; then 
-        ask "Enter the text [english alphabet only] [*, exit]"; # Ask for the text
+    if [[ ${@:$nextInput} == "" ]] || [[ $firstTime == 0 ]]; then 
+        ask "Enter the text [*, exit]"; # Ask for the text
         if [ "$askResponse" == "exit" ]; then
             break;
         fi
     else # If text given as argument
-        askResponse=$@; # Arguments are the text
+        askResponse=${@:$nextInput}; # Arguments are the text
     fi
 
     if [[ ! "$askResponse" =~ $reEx ]]; then
@@ -76,12 +126,7 @@ while [ 1 ]; do
     
     for (( i = 0; i < ${#askResponse}; i++ )); do
         echo -e "\""${LGREEN}"${askResponse:$i:1}"${NC}"\" is \"${conversor[${askResponse:$i:1}]}\"";
-        if [[ ${askResponse:$i:1} = " " ]] || [[ ${msg:(( ${#msg} - 1 )):1} = "|" ]]; then
-            # Because of space between words is | (7 spaces), do not add extra spaces in the sides
-            msg+="${conversor[${askResponse:$i:1}]}";
-        else
-            msg+=" ${conversor[${askResponse:$i:1}]}"; # 3 spaces = space between characters
-        fi
+        msg+="$spacer${conversor[${askResponse:$i:1}]}"; # Convert it to morse
     done
 
     firstTime=0;
