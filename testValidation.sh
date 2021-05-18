c5850fbdcd348183e2053fea0f53e1d3b79d1718
jkutkut <jorgeregonzalez@gmail.com>
2021-05-18 12:49:33 +0200

    logic ready for full test development (string to file comparison functional)

--- testValidation.sh
+++ testValidation.sh
@@ -1,11 +1,24 @@
 echo "Starting test";
-test1="abcdefghijklmnopkrstuvwxyz 0123456789";
+
 echo "Testing: $test1";
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo $__dir;
+
+test1="abcdefghijklmnopkrstuvwxyz 0123456789";
+# spectedResult='.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. -.- .-. ... - ..- ...- .-- -..- -.-- --..   ----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.';
+spectedResult="hfdjs"
+
+# Get current directory
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 
 
-( . ./ttM.sh $test1;)
-echo "done $msg"
-echo ${morseR};
-# echo $morseR
+( . ./ttM.sh $test1;) > outputTemp.txt;
+
+echo $(cat outputTemp.txt);
+echo $spectedResult
+
+
+if [ ! "$(cat outputTemp.txt)" == "$spectedResult" ]; then
+    echo "Does not match test1";
+fi
+
+
+echo "End of testing";
